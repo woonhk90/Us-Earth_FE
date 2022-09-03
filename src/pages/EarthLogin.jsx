071f4a7a921f0f820da08af1b578c0f3a92e5d77
woonhk90 <wonho7344@gmail.com>
2022-09-03 20:17:22 +0900

    [fix] 로그인 와이어프레임 완료

--- src/pages/EarthLogin.jsx
+++ src/pages/EarthLogin.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import LoginTop from '../components/LoginTop';
+import Layout from '../components/layout/Layout';
 import Login from '../components/Login';
+import CommunityBottom from '../components/CommunityBottom';
+import styled from 'styled-components';
 
 const EarthLogin = () => {
     return (
-        <Login />
+        <Layout>
+            <Header>
+                <LoginTop />
+            </Header>
+            <LoginBox>
+                <Login />
+            </LoginBox>
+            <Footer>
+                <CommunityBottom />
+            </Footer>
+        </Layout>
     )
 }
 export default EarthLogin;
+
+const LoginBox = styled.div`
+  height:100%;
+  overflow:auto;
+  position:absolute;
+  top:48px;
+  left:0;
+`;
