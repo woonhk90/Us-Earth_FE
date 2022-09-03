34c4f0b50030c8b8d463a00675d3805105691b72
loveyoujgb <kimmiri235@daum.net>
2022-09-03 16:51:38 +0900

    [fix]CommunityForm 사진 업로드 코드 수정

--- src/components/CommunityForm.jsx
+++ src/components/CommunityForm.jsx
@@ -10,15 +10,12 @@ import { useMutation, useQuery, useQueryClient } from "react-query";
 import { postCommunityFormData } from "../hooks/Query";
 
 const CommunityForm = () => {
-  const [files, setFiles] = useState([]);
   const [inputData, inputOnChangeHandler, inputReset] = useInputs({
     title: "",
     content: "",
   });
 
   const { title, content } = inputData;
-  console.log(files);
-
   //query
   const queryClient = useQueryClient();
   const { mutate: addMutateData } = useMutation((data) => postCommunityFormData(data), {
@@ -48,54 +45,53 @@ const CommunityForm = () => {
     };
   }, []);
 
-  //사진
-  const { fileRejections, getRootProps, getInputProps } = useDropzone({
-    maxFiles: 5,
-    maxSize: 20000000, //20메가
-    accept: {
-      "image/jpeg": [],
-      "image/png": [],
-    },
+  /* ---------------------------------- 사진 업로드 ---------------------------------- */
 
-    onDrop: (acceptedFiles) => {
-      setFiles(
-        acceptedFiles.map((file) =>
-          Object.assign(file, {
-            preview: URL.createObjectURL(file),
-          })
-        )
-      );
-    },
-  });
+  const [files, setFiles] = useState([]);
+  const [previewImg, setPreviewImg] = useState([]);
+  const [isPhotoMessage, setIsPhotoMessage] = useState("");
+  const [isPhoto, setIsPhoto] = useState(true);
 
-  const fileRejectionItems = fileRejections.map(({ file, errors }) => (
-    <div key={file.path}>
-      {errors.map((e) => {
-        return (
-          <div style={{ marginLeft: "10px" }} key={e.code}>
-            {e.code}
-          </div>
-        );
-      })}
-    </div>
-  ));
+  const addImageFile = (e) => {
+    if (e.target.files.length + files.length < 6) {
+      for (let i = 0; i < e.target.files.length; i++) {
+        if (e.target.files[i].size < 2000000) {
+          // 20메가
+          console.log(e.target.files[i].size);
+          const reader = new FileReader();
+          reader.readAsDataURL(e.target.files[i]);
+          reader.onload = () => {
+            const previewImgUrl = reader.result;
+            setPreviewImg((previewImg) => [...previewImg, previewImgUrl]);
+          };
+          const currentFiles = e.target.files[i];
+          setFiles((files) => [...files, currentFiles]);
+          setIsPhotoMessage("");
+        } else {
+          setIsPhotoMessage("파일이 너무 큽니다. 20MB미만의 파일만 업로드 됩니다.");
+          console.log(`${i}번째`, e.target.files[i].size);
+        }
+      }
+    } else {
+      setIsPhotoMessage("사진은 5장까지만 업로드 가능합니다.");
+      setIsPhoto(false);
+    }
+  };
 
-  const thumbs = files.map((file) => (
-    <Thumb key={file.name}>
-      <Img
-        src={file.preview}
-        // Revoke data uri after image is loaded
-        onLoad={() => {
-          URL.revokeObjectURL(file.preview);
-        }}
-      />
-    </Thumb>
-  ));
+  console.log(previewImg);
+  console.log(files);
+
+  // X버튼 클릭 시 이미지 삭제
+  const deleteImageFile = (index) => {
+    console.log(index);
+    setIsPhotoMessage("");
+    setPreviewImg(previewImg.filter((file, id) => id !== index));
+    setFiles(files.filter((file, id) => id !== index));
+  };
 
   const submitHandler = () => {
     let formData = new FormData();
     if (title === "") {
-      alert("제목을 입력해 주세요");
     } else if (content === "") {
       alert("내용을 입력해 주세요");
     } else if (files.length === 0) {
@@ -115,68 +111,47 @@ const CommunityForm = () => {
 
   return (
     <>
-      <div>인증 게시글 작성</div>
-      <div>이미지</div>
-      <Container>
-        <input {...getInputProps()} />
-        <StButton {...getRootProps()}>
-          <Length>{files.length}/5</Length>
-        </StButton>
-        <ThumbsContainer>{thumbs}</ThumbsContainer>
-        <div>{fileRejectionItems}</div>
-      </Container>
-      <div>그룹제목</div>
-      <input name="title" value={title} onChange={inputOnChangeHandler}></input>
-      <div>내용</div>
-      <textarea name="content" value={content} onChange={inputOnChangeHandler}></textarea>
-      <button onClick={submitHandler}>등록</button>
+      <CommunityFormWrap>
+        <div>인증 게시글 작성</div>
+        <div>이미지</div>
+        <AddPhotoWrap>
+          <Stform encType="multipart/form-data">
+            <StLabel htmlFor="file">
+              <StIcon>○</StIcon>
+            </StLabel>
+            <StImageInput multiple type="file" id="file" accept="image/*" onChange={(e) => addImageFile(e)} />
+            {/* <input type="file" id="file" accept="image/jpg, image/jpeg, image/png" onChange={(e) => addImageFile(e)} /> */}
+          </Stform>
+          <div>
+            {previewImg.map((image, index) => {
+              return (
+                <Container key={index}>
+                  <StButton key={index} onClick={() => deleteImageFile(index)}>
+                    버튼
+                  </StButton>
+                  <Thumb src={image} alt="img" />
+                </Container>
+              );
+            })}
+          </div>
+        </AddPhotoWrap>
+
+        <div>{isPhotoMessage}</div>
+        <div>제목</div>
+        <input name="title" value={title} onChange={inputOnChangeHandler}></input>
+        <div>내용</div>
+        <textarea name="content" value={content} onChange={inputOnChangeHandler}></textarea>
+        <button onClick={submitHandler}>등록</button>
+      </CommunityFormWrap>
     </>
   );
 };
 
 export default CommunityForm;
 
-//사진
-const StButton = styled.div`
-  cursor: pointer;
-  :hover {
-    border: 1px solid #999999;
-  }
-  width: 80px;
-  height: 80px;
-  background-color: #f1f1f1;
-  border: 1px solid #cccccc;
-  border-radius: 10px;
-  padding: 18px 23px;
-  position: relative;
-`;
-
-const Thumb = styled.div`
-  display: inline-flex;
-  width: 80px;
-  height: 80px;
-  padding: 4px;
-  box-sizing: border-box;
-`;
-
-const Container = styled.section`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  margin-bottom: 25px;
-`;
-
-const Length = styled.span`
-  font-size: 12px;
-  position: absolute;
-  bottom: 10px;
-  left: 31px;
-`;
-
-const ThumbsContainer = styled.aside`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+const CommunityFormWrap = styled.div`
+  width: 100%;
+  height: 100%;
 `;
 
 const Img = styled.img`
@@ -185,4 +160,60 @@ const Img = styled.img`
   width: auto;
   height: 100%;
   margin-left: 5px;
+`;
+
+//사진
+
+const AddPhotoWrap = styled.div`
+  /* display: flex;
+  justify-content: flex-start;
+  background-color: green;
+  width: 100%; */
+`;
+
+const Stform = styled.form`
+  /* display: inline-blosck;
+  justify-content: center;
+  align-items: center; */
+`;
+const StImageInput = styled.input`
+  position: absolute;
+  width: 0;
+  height: 0;
+  overflow: hidden;
+`;
+
+const StLabel = styled.label``;
+const StIcon = styled.div`
+  cursor: pointer;
+  :hover {
+    border: 1px solid #999999;
+  }
+  width: 100px;
+  height: 100px;
+  background-color: #f1f1f1;
+  border: 1px solid #cccccc;
+  border-radius: 10px;
+`;
+const StButton = styled.button`
+  /* position: absolute; */
+  right: -20px;
+  top: -15px;
+  z-index: 99;
+`;
+
+const Container = styled.div`
+  /* position: relative; */
+  width: 100px;
+  height: 100px;
+  margin: 5px;
+`;
+
+const Thumb = styled.img`
+  background-size: contain;
+  background-position: center;
+  border: 1px solid black;
+  border-radius: 12px;
+  width: 100px;
+  height: 100px;
 `;
