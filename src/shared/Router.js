51689b5cbeed8e33d2b2815eaa1527891c0a288b
loveyoujgb <kimmiri235@daum.net>
2022-09-07 21:44:49 +0900

    [fix] Community Form C 서버연결 완료

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -4,6 +4,7 @@ import KakaoLogin from "./KakaoLogin";
 import GoogleLogin from "./GoogleLogin";
 import NaverLogin from "./NaverLogin";
 import EarthCommunityForm from "../pages/EarthCommunityForm";
+import EarthCommunityFormEdit from "../pages/EarthCommunityFormEdit";
 import EarthCommunity from "../pages/EarthCommunity";
 import EarthCommunityDetail from "../pages/EarthCommunityDetail";
 import EarthCommunityCertify from "../pages/EarthCommunityCertify";
@@ -15,6 +16,7 @@ const Router = () => {
       <Route path="/" element={<EarthLogin />} />
       <Route path="/login" element={<EarthLogin />} />
       <Route path="/community/form" element={<EarthCommunityForm />} />
+      <Route path="/community/edit/:id" element={<EarthCommunityFormEdit />} />
       <Route path="/community" element={<EarthCommunity />} />
       <Route path="/community/detail/:id" element={<EarthCommunityDetail />} />
       <Route path="/community/certify" element={<EarthCommunityCertify />} />
