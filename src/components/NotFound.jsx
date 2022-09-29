0b4e3ebad3ca034a5a1c795b43117d5dca0b0d7e
loveyoujgb <kimmiri235@daum.net>
2022-09-30 01:29:31 +0900

    [style] 배너 및 notfound컴포넌트 디자인 수정

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -12,6 +12,7 @@ const NotFound = () => {
       <Layout>
         <NotFoundContainer>
           <NotFoundWrap>
+            <Span>404</Span>
             죄송합니다. 페이지가 존재하지 않습니다.
             <br />
             요청하신 페이지를 잘못 입력하셨거나, <br /> 페이지의 주소가 변경, <br /> 삭제되어 찾을 수 없습니다.
@@ -36,37 +37,52 @@ const NotFoundContainer = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
-  background-color: rgba(231, 255, 143, 0.2);
+  background-color: rgba(237, 251, 202, 0.2);
 `;
 
 const NotFoundWrap = styled.div`
   gap: 10px;
   ${flexColumn}
-  width: 100%;
+  width: 90%;
   text-align: center;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
   font-weight: 500;
-  font-size: 20px;
+  font-size: 18px;
   line-height: 27px;
   display: flex;
   align-items: center;
   letter-spacing: -0.03em;
-
+  padding: 55px 0 40px 0;
+  border-radius: 4px;
   color: #424242;
+`;
+const Span = styled.span`
+  padding-bottom: 40px;
+  width: 150px;
+  text-align: center;
+  font-weight: 800;
+  font-size: 110px;
+  line-height: 25px;
+  color: #73c600;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: -0.03em;
 `;
 
 const Button = styled.button`
-  border: none;
-  border-radius: 8px;
-  padding: 5px;
-  background: linear-gradient(180deg, #e4ffbd 0%, rgba(193, 250, 107, 0.7) 100%);
-  box-shadow: 1px 1px 1px 0px rgba(0, 0, 0, 0.2);
+  background-color: transparent;
+  border-radius: 3px;
+  padding: 4px;
+  margin-top: 5px;
+  color: #579400;
+  border: 2px solid #69b400;
   width: 150px;
   text-align: center;
-  font-weight: 300;
+  font-weight: 500;
   font-size: 18px;
   line-height: 25px;
   display: flex;
